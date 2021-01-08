0d493b762b57a67a54e36032c757be32bdda0ddb
Dimitri <dimitri.despert@gmail.com>
2021-01-08 16:30:59 +0100

    Mon CV V1.1 suppression du fichier idea et ajout du gitignore

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <?php
-    
+    $title = '';
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Despert D.</title>
+    <title><?php $title ?></title>
     <link rel="stylesheet" href="css/style.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.gstatic.com">
--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 
-<?php require('./header.php')?>
+<?php
+    require('./header.php');
+    $title = 'blablabla';
+?>
 
 <main>
     <section>
