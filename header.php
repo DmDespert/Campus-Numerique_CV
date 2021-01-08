6379f4b0991762aa68cd2f55d7d5f802076fbecc
Dimitri <dimitri.despert@gmail.com>
2021-01-08 16:07:32 +0100

    Modification des liens des pages dans le header

--- header.php
+++ header.php
@@ -11,9 +11,9 @@
     </div>
     <nav>
         <ul>
-            <li><a href="index.html">Accueil</a></li>
-            <li><a href="hobbies.html">Hobbies</a></li>
-            <li><a href="contact.html">Contact</a></li>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="hobbies.php">Hobbies</a></li>
+            <li><a href="contact.php">Contact</a></li>
         </ul>
     </nav>
 </header>
