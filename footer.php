1adb3c7be06ae3b59e197cc8da20095671ec6671
Dimitri <dimitri.despert@gmail.com>
2021-01-08 15:52:58 +0100

    Premiere sauvegarde du CV en header et footer PHP

---
+++ footer.php
@@ -0,0 +1,16 @@
+<?php
+
+
+?>
+<footer class="secondcolor">
+    <div class="allright">
+        <p>
+            <a href="#name" id="btnup\">Revenir en haut</a>
+            <br>
+            <b class="maincolor">Despert Dimitri </b>@All de mes droits riseurvaid
+            <br>
+            <a href="https://www.instagram.com/"><img src="img/logotwitter.jpg" alt="instagram" class="logo"></a>
+            <a href="https://twitter.com/"><img src="img/logoinsta.jpeg" alt="twitter" class="logo"></a>
+        </p>
+    </div>
+</footer>
---
+++ header.php
@@ -0,0 +1,19 @@
+<?php
+    echo 'Je suis du php !!!'
+?>
+<header>
+    <p id="name">Despert <strong>D.</strong></p>
+    <div class="photodeMwa"></div>
+    <div class="title">
+        <p id="first-title">CONCEPTION ET R&Eacute;ALISATION DE SITES INTERNETS,<br>APPLICATIONS WEB & MOBILE</p>
+        <p id="second-title">D&Eacute;VELOPPEUR WEB<br>FRONT-END & BACK-END</p>
+        <p id="region">VALENCE, MONT&Eacute;LIMAR ET SA R&Eacute;GION</p>
+    </div>
+    <nav>
+        <ul>
+            <li><a href="index.html">Accueil</a></li>
+            <li><a href="hobbies.html">Hobbies</a></li>
+            <li><a href="contact.html">Contact</a></li>
+        </ul>
+    </nav>
+</header>
